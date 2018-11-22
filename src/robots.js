281a3a9d93312365fe87f23bc263ad4d3955e6a9
hassan Ali <angel3r@hotmail.com>
2018-11-22 02:02:02 +0100

    Starter Files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hammfy Lolo',
+        username: 'Bobo',
+        email: 'bambocha@kk.se'
+    },
+    {
+        id: 2,
+        name: 'Amigo Nara',
+        username: 'Chacha',
+        email: 'chacha@emm.se'
+    },
+    {
+        id: 3,
+        name: 'Samo Solo',
+        username: 'Solo',
+        email: 'Samosolo@prob.se'
+    },
+    {
+        id: 4,
+        name: 'Salami Man',
+        username: 'Man',
+        email: 'Salami@kk.se'
+    },
+    {
+        id: 5,
+        name: 'Salami Man',
+        username: 'Man',
+        email: 'Salami@kk.se'
+    },
+    {
+        id: 6,
+        name: 'Salami Man',
+        username: 'Man',
+        email: 'Salami@kk.se'
+    },
+    {
+        id: 7,
+        name: 'Salami Man',
+        username: 'Man',
+        email: 'Salami@kk.se'
+    },
+    {
+        id: 8,
+        name: 'Salami Man',
+        username: 'Man',
+        email: 'Salami@kk.se'
+    },
+    {
+        id: 9,
+        name: 'Salami Man',
+        username: 'Man',
+        email: 'Salami@kk.se'
+    },
+    {
+        id: 10,
+        name: 'Salami Man',
+        username: 'Man',
+        email: 'Salami@kk.se'
+    },
+]
